88303ca3808da6ad274323f80a704548be05bdeb
Ghost_Type12 <joshuam36265@gmail.com>
2024-07-02 20:17:33 -0500

    working from other computer updated css

--- scripts/index.js
+++ scripts/index.js
@@ -30,23 +30,54 @@ let initialCards = [
   },
 ];
 
+const profileFormElement = document.querySelector('.modal__container');
+const formElement = document.querySelector('.modal__save');
+const nameInput = document.querySelector('#modal__input_name');
+const jobInput = document.querySelector('#modal__input_job');
+
+// find the profile elements in the DOM
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+
 let editButton = document.querySelector(".profile__edit");
 let addButton = document.querySelector(".profile__add");
 let overlay = document.querySelector(".overlay");
 let modal = document.querySelector(".modal");
-let closeModal = document.querySelector(".modal__close");
+let modalName = document.querySelector("#modal__input_name");
+let modalJob = document.querySelector("#modal__input_job");
+let closeModalBtn = document.querySelector(".modal__close");
 let saveButton = document.querySelector(".modal__save");
 
+closeModalBtn.addEventListener('click', closeModal);
 editButton.addEventListener('click', openModal );
-closeModal.addEventListener('click', openModal );
 
+
+// CLOSE MODAL
+function closeModal(){
+  modal.classList.remove("modal__opened");
+  overlay.classList.remove("modal__opened");
+}
+
+// OPEN MODAL
 function openModal() {
   if (!modal.classList.contains("modal__opened")){
     modal.classList.add("modal__opened");
     overlay.classList.add("modal__opened");
+    modalName.value = profileName.textContent;
+    modalJob.value = profileJob.textContent;
   } else {
-    modal.classList.remove("modal__opened");
-    overlay.classList.remove("modal__opened");
+    closeModal(profileFormElement)
   }
-
 }
+
+
+// MODAL FORM SUBMISSION
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = modalName.value;
+  console.log(profileName.value)
+  profileJob.textContent = modalJob.value;
+  console.log(profileJob.textContent, modalName.value)
+}
+
+formElement.addEventListener('submit', handleProfileFormSubmit);
