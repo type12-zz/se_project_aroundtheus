5ac997407a66120fd7539460104dcc77d5b6ccf3
Ghost_Type12 <joshuam36265@gmail.com>
2024-07-02 23:09:41 -0500

    fix modal not saving

--- scripts/index.js
+++ scripts/index.js
@@ -23,33 +23,33 @@ let initialCards = [
     name: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
-
+  
   {
     name: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
 
-const profileFormElement = document.querySelector('.modal__container');
-const formElement = document.querySelector('.modal__save');
-const nameInput = document.querySelector('#modal__input_name');
-const jobInput = document.querySelector('#modal__input_job');
 
 // find the profile elements in the DOM
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
-
-let editButton = document.querySelector(".profile__edit");
-let addButton = document.querySelector(".profile__add");
-let overlay = document.querySelector(".overlay");
-let modal = document.querySelector(".modal");
-let modalName = document.querySelector("#modal__input_name");
-let modalJob = document.querySelector("#modal__input_job");
-let closeModalBtn = document.querySelector(".modal__close");
-let saveButton = document.querySelector(".modal__save");
+const editButton = document.querySelector(".profile__edit");
+const addButton = document.querySelector(".profile__add");
+const overlay = document.querySelector(".overlay");
+const profileFormElement = document.querySelector('.modal__container');
+const formElement = document.querySelector('.modal__save');
+const modalNameInput = document.querySelector('#modal__input_name');
+const modalJobInput = document.querySelector('#modal__input_job');
+const modal = document.querySelector(".modal");
+const modalName = document.querySelector("#modal__input_name");
+const modalJob = document.querySelector("#modal__input_job");
+const closeModalBtn = document.querySelector(".modal__close");
+const saveButton = document.querySelector(".modal__save");
 
 closeModalBtn.addEventListener('click', closeModal);
 editButton.addEventListener('click', openModal );
+saveButton.addEventListener('click', handleProfileFormSubmit);
 
 
 // CLOSE MODAL
@@ -63,8 +63,8 @@ function openModal() {
   if (!modal.classList.contains("modal__opened")){
     modal.classList.add("modal__opened");
     overlay.classList.add("modal__opened");
-    modalName.value = profileName.textContent;
-    modalJob.value = profileJob.textContent;
+    modalNameInput.value = profileName.textContent;
+    modalJobInput.value = profileJob.textContent;
   } else {
     closeModal(profileFormElement)
   }
@@ -72,12 +72,9 @@ function openModal() {
 
 
 // MODAL FORM SUBMISSION
-function handleProfileFormSubmit(evt) {
+function handleProfileFormSubmit(evt){
   evt.preventDefault();
   profileName.textContent = modalName.value;
-  console.log(profileName.value)
   profileJob.textContent = modalJob.value;
-  console.log(profileJob.textContent, modalName.value)
+  closeModal();
 }
-
-formElement.addEventListener('submit', handleProfileFormSubmit);
