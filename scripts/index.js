1a0b6b28ea47b528843eb84cde676dbce8268659
Joshua Moten <joshuam36265@gmail.com>
2024-07-13 23:05:52 -0500

    add like function, delete/trash image, and their event handlers

--- scripts/index.js
+++ scripts/index.js
@@ -37,23 +37,44 @@ const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
 const editButton = document.querySelector(".profile__edit");
 const addButton = document.querySelector(".profile__add");
-const overlay = document.querySelector(".overlay");
+const cardImage = cardTemplate.querySelector(".card__image");
+
+const modal = document.querySelector(".modal");
 const profileFormElement = document.querySelector(".modal__container");
 const clickedModalInputs = document.querySelectorAll(".modal__input");
 const modalNameInput = document.querySelector("#modal__input_name");
 const modalJobInput = document.querySelector("#modal__input_job");
-const modal = document.querySelector(".modal");
-const modalName = document.querySelector("#modal__input_name");
-const modalJob = document.querySelector("#modal__input_job");
 const closeModalBtn = document.querySelector(".modal__close");
 const formSaveButton = document.querySelector(".modal__save");
 
+
+const addCardModal = document.querySelector(".add_modal");
+const addCardFormContainerElement = document.querySelector(".add_modal__container");
+const closeAddCardModalBtn = document.querySelector(".add_modal__close");
+const addCardModalFormElement = document.querySelector(".add_modal__form");
+const clickedAddModalFormCardInputs = document.querySelectorAll(".add_modal__input");
+const addModalCardTitle = addCardModalFormElement.querySelector("#add_modal__input_title");
+const addModalCardUrl = addCardModalFormElement.querySelector("#add_modal__input_url");
+const formCardSaveButton = addCardModalFormElement.querySelector(".modal__save");
+
+const likeCardButton = document.querySelector(".card__like");
+const deleteCardButton = cardsList.querySelector(".card__trash_image");
+
+
+const modalName = document.querySelector("#modal__input_name");
+const modalJob = document.querySelector("#modal__input_job");
+
 //EVENT HANDLERS
 closeModalBtn.addEventListener("click", closeModal);
+closeAddCardModalBtn.addEventListener("click", closeAddCardModal);
 editButton.addEventListener("click", openEditProfileModal);
+addButton.addEventListener("click", openAddCardModal);
 formSaveButton.addEventListener("click", handleProfileFormSubmit);
-// clickedModalInputs.forEach((input) => {
-//   input.addEventListener("click", clearModalInputField);
+formCardSaveButton.addEventListener("click", handleAddCardFormSubmit);
+cardsList.addEventListener("click", handleLikeCard);
+cardsList.addEventListener("click", handleDeleteCard);
+// cardsList.addEventListener("click", () => {
+//   console.log(`${data.name} card was clicked`);
 // });
 
 // **********JAVASCRIPT FUNCTIONS*********
@@ -61,6 +82,10 @@ formSaveButton.addEventListener("click", handleProfileFormSubmit);
 // CLOSE MODAL
 function closeModal() {
   modal.classList.remove("modal_opened");
+}
+
+function closeAddCardModal() {
+  addCardModal.classList.remove("add_modal_opened");
 }
 
 // OPEN MODAL
@@ -71,10 +96,23 @@ function openEditProfileModal() {
   }
 }
 
+function openAddCardModal() {
+  if (!addCardModal.classList.contains("add_modal_opened")) {
+    addCardModal.classList.add("add_modal_opened");
+    fillCardForm();
+  }
+}
+
 //  FILL PROFILE FORM
 function fillProfileForm() {
   modalNameInput.value = profileName.textContent;
   modalJobInput.value = profileJob.textContent;
+}
+
+//  FILL CARD FORM
+function fillCardForm() {
+  addModalCardTitle.value = addModalCardTitle.textContent;
+  addModalCardUrl.value = addModalCardUrl.textContent;
 }
 
 // MODAL FORM SUBMISSION
@@ -85,6 +123,21 @@ function handleProfileFormSubmit(evt) {
   closeModal();
 }
 
+// MODAL ADD CARD FORM SUBMISSION
+function handleAddCardFormSubmit(evt) {
+  console.log("Hello")
+  evt.preventDefault();
+  const newCardData = {
+    name: addModalCardTitle.value,
+    link: addModalCardUrl.value,
+  };
+
+  initialCards.push(newCardData);
+  const cardElement = createCard(newCardData)
+  cardsList.prepend(cardElement);
+  closeAddCardModal();
+}
+
 // CLEAR MODAL INPUT FIELDS
 function clearModalInputField(evt) {
   const target = evt.target;
@@ -93,38 +146,41 @@ function clearModalInputField(evt) {
   }
 }
 
-// CREATE CARD
-// function getCardElement() {
-//   for (let item=0; item < initialCards.length; item++){
-//     // loop through initalCard
-//     let profileCard = `
-//       <li class="card">
-//         <img
-//           src="${initialCards[item].link}"
-//           alt="image of ${firstWords[item]}"
-//           class="card__image"
-//         />
-//         <div class="card__description">
-//           <h2 class="card__title">${initialCards[item].name }</h2>
-//           <button type="button" class="card__like"></button>
-//         </div>
-//       </li>
-//     `
-//     const newCard = document.createElement("li");
-//     newCard.innerHTML = profileCard;
+// LIKE CARD FUNCTION
+function handleLikeCard(evt) {
+  evt.preventDefault();
+  const cardElement = evt.target.closest(".card");
+  if (cardElement) {
+    if (evt.target.classList.contains("card__like")) {
+      const likeButton = cardElement.querySelector(".card__like")
+      likeButton.classList.toggle("card__like_active");
+      console.log("I activated");
+    }
+  }
+}
 
-//     // Place newly created card in cardList
-//     cardList.appendChild(newCard);
-//   }
-// }
+//DELETE CARD FUNCTION
+function handleDeleteCard(evt) {
+  evt.preventDefault();
+  const cardElement = evt.target.closest(".card");
+  if (cardElement) {
+    if(evt.target.classList.contains("card__trash_image")) {
+      const cardElement = evt.target.closest(".card");
+      cardsList.removeChild(cardElement);
+    }
+  }
+}
 
 // CREATE CARDS
 function createCard(data) {
+  // console.log("I am in Create Card");
   const cardElement = cardTemplate.cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
+  const cardTrashImage = cardElement.querySelector(".card__trash_image");
   const cardTitle = cardElement.querySelector(".card__title");
-  const firstWords = data.name.split(" ")[0];
+  const firstWords = data.name.split("  ");
   cardImage.src = data.link;
+  cardTrashImage.src = "../images/active-trash.svg";
   cardImage.alt = `image of ${firstWords}`;
   cardTitle.textContent = data.name;
 
